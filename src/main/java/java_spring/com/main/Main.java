15271be1769d9a23876b7c4c8e5115061812fa1c
Amar giri <amargoswami9520@gmail.com>
2026-07-15 15:02:35 +0530

    update

--- src/main/java/java_spring/com/main/Main.java
+++ src/main/java/java_spring/com/main/Main.java
@@ -8,18 +8,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		ApplicationContext ioc = new ClassPathXmlApplicationContext("spring.xml");
+		ApplicationContext ioc = new ClassPathXmlApplicationContext("inner-bean.xml");
 		
 		Employee bean = ioc.getBean("emp1", Employee.class);
 		
 		System.out.println(bean);
-		
-		Employee bean2 = ioc.getBean("emp2",Employee.class);
-		
-		System.out.println(bean2);
-		
-		Employee bean3 = ioc.getBean("emp3",Employee.class);
-		
-		System.out.println(bean3);
+	
 	}
 }
